18a50fefd3f6800d79315d55ac3be9dcd4da1b41
Marcel <marcel44.gruszecki@gmail.com>
2025-03-29 15:37:14 +0100

    Version 1

--- Header.h
+++ Header.h
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "json.hpp"
+using json = nlohmann::json;
 
+json database();
 void add(std::string);
 void update(int id, std::string);
+void remove(int id); 
+void mark_in_progress(int id);
+void mark_done(int id);
+void list();
+void list_todo();
+void list_in_progress();
 std::string get_current_date();
